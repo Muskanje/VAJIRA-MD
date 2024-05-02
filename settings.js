7d6305d1a2db24f825db8b4c5764df78debccedc
Muskanje <100859453+Muskanje@users.noreply.github.com>
2024-05-02 18:47:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,47 +7,47 @@ global.email = 'vajirarathnayaka891@gmail.com'
 global.github = 'https://github.com/VajirTech/VAJIRA-MD'
 global.location = 'Srilanka Colombo'
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
+global.author = 'Faisal 𝙈𝘿';
 global.mess = {
-    banned: '`You are *Banned* fron using commands!`',
+    banned: '`You are *Banned* fron using commands!`lan',
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
     botAdmin: '`I am not an admin!`',
     owner: '`You are not my owner`',
     group: '`You can use this command only in groups ❌`',
-    private: '`Feature is used only for private chats!`',
+    private: '`Feature is used only for private chats!`i love you',
     bot: '`Bot number user special features`',
     wait: '*Processing Your request*',
-    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 24 hours`',
 }
 
-global.owner = [process.env.OWNER_NUMBER] || ['94711453097'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = [process.env.OWNER_NUMBER] || ['923037484167'] // Owner number , (923112657705)
 
 module.exports = {    
 SESSION_ID: process.env.SESSION_ID || 'youre session id',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 LANG:  process.env.LANG || 'SI' ,    
-ANTI_BAD: process.env.ANTI_BAD || false  ,
-AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
+ANTI_BAD: process.env.ANTI_BAD || true ,
+AUTO_REACT:  process.env.AUTO_REACT  || true ,    
 AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
-AUTO_RECORDING:  process.env.AUTO_RECORDING  || false  ,
-AUTO_READ:  process.env.AUTO_READ  || false  ,
+AUTO_RECORDING:  process.env.AUTO_RECORDING  || true,
+AUTO_READ:  process.env.AUTO_READ  || true  ,
 AUTO_BIO:  process.env.AUTO_BIO  || false  ,    
 ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,            
-WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
-ANTI_LINK: process.env.ANTI_LINK || false  ,
-ANTI_BOT: process.env.ANTI_BOT || false  ,
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94719199757'  ,    
+WORK_TYPE: process.env.WORK_TYPE || 'public'  ,
+ANTI_LINK: process.env.ANTI_LINK || true  ,
+ANTI_BOT: process.env.ANTI_BOT || true  ,
+OWNER_NUMBER: process.env.OWNER_NUMBER || '923037484167'  ,    
 ALIVE: process.env.ALIVE || `default`,
-OWNER_NAME: process.env.OWNER_NAME || 'Vajira' ,     
-AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
-AUTO_VOICE:  process.env.AUTO_VOICE  || false  ,
-AUTO_STICKER: process.env.AUTO_STICKER || false  ,
+OWNER_NAME: process.env.OWNER_NAME || 'Faisal udas' ,     
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true ,    
+AUTO_VOICE:  process.env.AUTO_VOICE  || true,
+AUTO_STICKER: process.env.AUTO_STICKER || true,
 WELCOME:  process.env.WELCOME  || false ,    
 NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || false  ,
-FOOTER: process.env.FOOTER ||  '©ＶＡＪＩＲＡ - ＭＤ - Ｖ4',
-LOGO: process.env.LOGO || `https://telegra.ph/file/03f1eccdcb525a5e1a6ad.jpg` ,
-IMAGE: process.env.IMAGE || `https://telegra.ph/file/03f1eccdcb525a5e1a6ad.jpg`     
+FOOTER: process.env.FOOTER ||  'Faisal udas - ＭＤ - Ｖ4',
+LOGO: process.env.LOGO || `https://telegra.ph/file/320320577b88760c89210.jpg` ,
+IMAGE: process.env.IMAGE || `https://telegra.ph/file/320320577b88760c89210.jpg`     
 };
